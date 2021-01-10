20e4af68c98e8b8aabe22d540030785a3c10fb5c
xinbo <57312852+iiiiiiiiilll@users.noreply.github.com>
2021-01-10 19:43:35 +0800

    Add files via upload

---
+++ gplearn_example.py
@@ -0,0 +1,22 @@
+from gplearn.genetic import SymbolicRegressor
+from sklearn.utils.random import check_random_state
+import numpy as np
+rng = check_random_state(0)
+
+# Training samples
+X_train = rng.uniform(-1, 1, 100).reshape(50, 2)
+y_train = X_train[:, 0]**2 - X_train[:, 1]**2 + X_train[:, 1] - 1
+
+# Testing samples
+X_test = rng.uniform(-1, 1, 100).reshape(50, 2)
+y_test = X_test[:, 0]**2 - X_test[:, 1]**2 + X_test[:, 1] - 1
+est_gp = SymbolicRegressor(population_size=5000,
+                           generations=20, stopping_criteria=0.01,
+                           p_crossover=0.7, p_subtree_mutation=0.1,
+                           p_hoist_mutation=0.05, p_point_mutation=0.1,
+                           max_samples=0.9, verbose=1,
+                           parsimony_coefficient=0.01, random_state=0)
+est_gp.fit(X_train, y_train)
+print(est_gp._program)
+dot_data = est_gp._program.export_graphviz()
+print(est_gp._program.parents)
